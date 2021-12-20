995174b385cc6c34455a7d0235844a42552e57d3
RinatMans <91965034+RinatMans@users.noreply.github.com>
2021-12-20 14:44:52 +0500

    Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat oneCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat twoCat =
+                (Cat) applicationContext.getBean("cat");
+        boolean hwBollean = bean == bean2;
+        boolean catBollean = oneCat == twoCat;
+        System.out.println(hwBollean);
+        System.out.println(catBollean);
+
         System.out.println(bean.getMessage());
     }
 }
